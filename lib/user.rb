caaca9f91d1eb90722d994b65b9207e827d29260
AdamBrownBB <adam.brown.betty@gmail.com>
2019-09-13 11:08:57 -0400

    mvp

--- lib/user.rb
+++ lib/user.rb
@@ -20,15 +20,33 @@ class User < ActiveRecord::Base
         UserInterest.create(user_id: self.id, interest_id: new_ui.id)
     end
 
+    def get_my_ui
+        UserInterest.all.select {|ui| ui.user == self}
+    end
+
     def delete_user_interest
         prompt = TTY::Prompt.new
-        deletion_choices = self.interests.map {|interest| interest.name}
-        deletion = prompt.select("Would you like to be less interested in anything?", deletion_choices)
+        yes_no = ["Yes", "Nah"]
+        delete_or_not = prompt.select("Do you want to clear your choices?", yes_no)
+         if delete_or_not == "Yes"
+            kill_these = get_my_ui
+            kill_these.map {|ui| ui.delete}
+            puts "You dead"
+            # binding.pry
+         else puts "try again"  
+    
+        #     my_interests = self.user_interests.id
+        #     delete()
+        #  else answer == "View my Interests"
+            # need to display list of this users' interests
+         end   
+        # deletion_choices = self.interests.map {|interest| interest.name}
+        # deletion = prompt.select("Would you like to be less interested in anything?", deletion_choices)
 
-        test_var = self.user_interests
-        binding.pry
-        test_var.delete("name" => deletion)
-        binding.pry
+        # test_var = self.user_interests
+        # binding.pry
+        # test_var.delete("name" => deletion)
+        # binding.pry
     end
 
     
